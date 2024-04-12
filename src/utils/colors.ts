feb926a05d79241847b22403f2e0ff9b1b1d5239
Alexey Immoreev <lexx92@mail.ru>
2024-04-12 17:44:18 +0400

    Refactor: move colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,1 +1,10 @@
 export type Color = "white" | "yellow" | "black" | "green" | "peach";
+
+export const colors: Record<Color | "transparent", string> = {
+  yellow: "#F9B61A",
+  white: "#FFF",
+  black: "#000",
+  green: "#0E9C49",
+  peach: "#ED9C61",
+  transparent: "rgba(0,0,0,0)",
+};
