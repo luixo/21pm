1b4db675f3f51bca7ba5f528f9a0801353af2a8b
Alexey Immoreev <lexx92@mail.ru>
2024-04-12 17:44:18 +0400

    Refactor: move colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,5 @@
 import type { Config } from "tailwindcss";
-import type { Color } from "./src/utils/colors";
-
-const colors: Record<Color | "transparent", string> = {
-  yellow: "#F9B61A",
-  white: "#FFF",
-  black: "#000",
-  green: "#0E9C49",
-  peach: "#ED9C61",
-  transparent: "rgba(0,0,0,0)",
-};
+import { colors } from "./src/utils/colors";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
